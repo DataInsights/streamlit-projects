f85fd136020751e9d983d0314ff990d86265d14d
tmbuza <ndelly@gmail.com>
2022-08-28 01:32:42 -0500

    Add new files

--- stock.py
+++ stock.py
@@ -4,35 +4,27 @@
 
 import streamlit as st
 """
-# Python and Steamlit Simple Apps
-## Integrated practical user guides
+# Python and Streamlit Simple Apps
 """
 
 import yfinance as yf
 import pandas as pd
-import yfinance as yf
 
-
-## Quick glimpse
 
 """
-We will work with stock data and we can get started by defining the ticker symbol. Here we choose `GOOGL` like so:
+##  `GOOGL` stock data from 2010 to 2020
+
 """
 tickerSymbol = 'GOOGL'
 
-"""
-Now, extract data from the ticker and store it in an object named `tickerData`.
-"""
+# Now, extract data from the ticker and store it in an object named `tickerData`.
 tickerData = yf.Ticker(tickerSymbol)
 
-"""
-Get prices for for the past ten years.
-"""
 
+# Get prices for for the past ten years.
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-1-31')
 
 """
-## Data structure
 ![](img/stock.png)
 """
 
@@ -41,23 +33,22 @@ stockhead = pd.read_csv('stockhead.csv')
 stockhead
 
 """
-### Line charts for **Open**
+### Line charts for `Open`
 """
 st.line_chart(tickerDf.Open)
 
 """
-### Line charts for **Close**
+### Line charts for `Close`
 """
 st.line_chart(tickerDf.Close)
 
 """
-### Line charts for **Volume**
+### Line charts for `Volume`
 """
 st.line_chart(tickerDf.Volume)
 
 
 """
-### Line charts for **Four Features**
+### Line charts for Four Features
 """
 st.line_chart(data = tickerDf.iloc[:, 0:4])
-
